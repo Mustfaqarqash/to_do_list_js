32c8e64200408358e32fb3ebd473a564417e4027
Mustfaqarqash <133997345+Mustfaqarqash@users.noreply.github.com>
2024-06-09 17:40:56 +0300

    the end

--- script.js
+++ script.js
@@ -1,48 +1,104 @@
-let taks = [
+let tasksContainer = document.getElementById("tasks");
+let taskTemplate = document.getElementById("template-task");
+let addButton = document.getElementById("add-button");
+let tasks = [
     {
         title: "Read Book",
         date: "8/6/2024",
         isDone: true,
     },
-    {
-        title: "Start project",
-        date: "8/6/2024",
-        isDone: false,
-    },
-    {
-        title: "End project",
-        date: "8/6/2024",
-        isDone: true,
-    }
+
 
 ];
-let task = {
-    title: "Read Book",
-    date: "8/6/2024",
-    isDone: true,
-};
-document.getElementById("tasks").innerHTML ="";
-for (let i = 0; i < taks.length; i++) {
-    document.getElementById("tasks").innerHTML += `
-    <section>
-           <div class="buttons">
-               <button class="update">
-                   <i class="fa-regular fa-pen-to-square"></i>
-               </button>
-               <button class="success">
-                   <i class="fa-solid fa-check"></i>
-               </button>
-               <button class="delete">
-                   <i class="fa-solid fa-trash"></i>
-               </button>
-           </div>
-           <div class="content">
-               <p class="title">${taks[i].title}</p>
-               <div class="date">
-                   <p>${taks[i].date}</p>
-                   <i class="fa-regular fa-calendar"></i>
-               </div>
-           </div>
-       </section>
-`
+function getTasksFromStorage() {
+    let myTasksFromStrage = JSON.parse(localStorage.getItem("myTasks"));
+    if ( myTasksFromStrage != null) {
+        tasks = myTasksFromStrage;
+    }
+}
+getTasksFromStorage();
+// -----------fill Tasks In------------------------
+function getTasks() {
+    tasksContainer.innerHTML = "";
+    for (let i = 0; i < tasks.length; i++) {
+        let content = tasks[i];
+        let newTaskSection = taskTemplate.cloneNode(true);
+
+        let taskTitle = newTaskSection.querySelector(".task-title");
+        let taskDate = newTaskSection.querySelector(".task-date");
+        let successButton = newTaskSection.querySelector(".success-button");
+        let deleteBtton = newTaskSection.querySelector(".delete-button");
+        let updateButton = newTaskSection.querySelector(".update-button");
+        taskTitle.innerText = content.title;
+        taskDate.innerText = content.date;
+        if (content.isDone) {
+            successButton.style.backgroundColor = "rgb(242, 135, 5)";
+            // successButton.style.color = "white";
+            successButton.innerHTML = `<i class="fa-regular fa-circle-xmark"></i>`;
+            newTaskSection.style.backgroundColor = "rgba(15, 219, 15, 0.445)"
+        }
+        // -----------------Events-----------------------------------
+
+        successButton.addEventListener("click", () => {
+            isSuccess(i);
+        });
+
+        deleteBtton.addEventListener("click", () => {
+            deleteTask(i);
+        });
+
+        updateButton.addEventListener("click", () => {
+            updateTask(i);
+        })
+        //   --------------------------------------------------------
+        tasksContainer.appendChild(newTaskSection);
+
+    }
+
+}
+// -----------Storage Function------------------------
+function storeTasks() {
+    let tasksString = JSON.stringify(tasks);
+    localStorage.setItem("myTasks", tasksString);
+}
+// -----------Add Tasks------------------------
+addButton.addEventListener("click", () => {
+    let taskName = prompt("pleas enter task title");
+    let now = new Date();
+    let dateNow = now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear() + " - " + (now.getHours() - 12) + ":" + now.getMinutes();
+    let taskData = {
+        title: taskName,
+        date: dateNow,
+        isDone: false,
+    };
+    tasks.push(taskData);
+    storeTasks();
+    getTasks();
+
+});
+// -----------Delete Tasks------------------------
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    storeTasks();
+    getTasks();
+}
+// -----------Success Function------------------------
+function isSuccess(index) {
+    if (tasks[index].isDone) {
+        tasks[index].isDone = false;
+    } else tasks[index].isDone = true;
+    storeTasks();
+    getTasks();
+}
+// -----------Update Task------------------------
+function updateTask(index) {
+    let taskName = prompt("pleas enter task title");
+    let now = new Date();
+    let dateNow = now.getDate() + "/" + (now.getMonth() + 1) + "/" + now.getFullYear() + " - " + (now.getHours() - 12) + ":" + now.getMinutes();
+    if (taskName != "") {
+        tasks[index].title = taskName;
+        tasks[index].date = dateNow;
+    }
+    storeTasks();
+    getTasks();
 }
